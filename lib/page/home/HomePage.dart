1234c41345b81406a7749dfafc99382e680c579c
wuyanqiang <wuyanqiang@inke.cn>
2020-01-06 20:50:38 +0800

    直播列表 图片加载

--- lib/page/home/HomePage.dart
+++ lib/page/home/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:zqplayerflutter/page/home/model/live_item.dart';
 import 'package:zqplayerflutter/utils/NetUtil.dart';
@@ -10,46 +11,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _PageState extends State<HomePage> {
-  
+
   @override
   void initState() {
     // TODO: implement initState
     NetUtil.post("/live/list", {"offset": 0, "limit": 20, "game_type": "ow"})
         .then((response) {
-      print("home ::::  ");
-      List<LiveItem> list = getLiveItemList(response.data);
-      print(list);
+      setState(() {
+        list = getLiveItemList(response.data);
+      });
     }).catchError((e) {
       print(e);
     });
     super.initState();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _getBody(),
+      body: SafeArea(child: _getBody()), //
     );
   }
 
+  List<LiveItem> list = List();
+
   Widget _getBody() {
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-              height: 300.0,
-              width: 300.0,
-              color: Colors.red,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      color: Colors.blue,
-                    )
-                  ])),
-        ]);
+    return new ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return _getLiveItem(list[index]);
+      },
+      itemCount: list.length,
+    );
+  }
+
+  Widget _getLiveItem(LiveItem item) {
+    return Padding(padding: EdgeInsets.all(10.0),
+      child: Column(children: <Widget>[
+//        Image.network(item.liveImg),
+        new CachedNetworkImage(imageUrl: item.liveImg),
+        Text("${item.liveTitle}")
+      ],),
+    );
   }
 
 }
